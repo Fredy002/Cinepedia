6e92c77b840fe906b3a697e30d9913002dfc224e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-11-03 22:25:26 -0500

    loading screen incorporated in the main screen

--- lib/src/screens/movies/home_screen.dart
+++ lib/src/screens/movies/home_screen.dart
@@ -37,15 +37,16 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
     final slideShowMovies = ref.watch(moviesSlideShowProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final upcomingMovies = ref.watch(upcomingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
 
     if (slideShowMovies.isEmpty) return const CircularProgressIndicator();
-
-    return FullScreenLoader();
 
     return CustomScrollView(slivers: [
       const SliverAppBar(
